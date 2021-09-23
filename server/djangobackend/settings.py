8e7813257c8b2388a1975019ca6869d2a089c69c
Liran Zuk <jozuk97@hotmail.co.il>
2021-09-23 21:50:22 +0300

    ALLOWED_HOSTS

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,8 +27,9 @@ DEBUG = True
 
 # ALLOWED_HOSTS = ["localhost"]
 ALLOWED_HOSTS = ["127.0.0.1",
-    "esz-supercars.mybluemix.net"]
-
+    "esz-supercars.mybluemix.net",
+    "localhost:8000",
+    "localhost"]
 
 # Application definition
 
